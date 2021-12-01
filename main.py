17a36685d0a1a504da2fd17ab9e2861a17a7c04f
sb2539 <sinb1000@naver.com>
2021-12-01 16:09:25 +0900

    Add tokenize sample sentence and variables

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ import math, copy, time
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import seaborn
+import spacy
 seaborn.set_context(context="talk")
 ##%matplotlib inline
 
 
+d_model = 512
+text = "I am a student"
+spacy_en = spacy.load('en_core_web_sm') # using full name 'en' -> 'en_core_web_sm'
+dropout = 0.1
+
+def tokenize(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+vocab = tokenize(text)
+print(vocab, len(vocab))
 
 ## Word embedding class
+
 class Embeddings(nn.Module) :
     def __init__(self, d_model, vocab):
         super(Embeddings, self).__init__()
@@ -20,3 +32,5 @@ class Embeddings(nn.Module) :
 
     def forward(self, x):
         return self.lut(x) * math.sqrt(self.d_model) # multiply sqrt(d_model) to embeded result
+
+
